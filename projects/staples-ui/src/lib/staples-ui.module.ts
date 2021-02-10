c74978b86d795932a3e1f4c404d186c18ba3a394
Jose Bustamante <bercopy@gmail.com>
2021-02-10 14:40:56 +0100

    UI - Added Export Button + CommonModule

--- projects/staples-ui/src/lib/staples-ui.module.ts
+++ projects/staples-ui/src/lib/staples-ui.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { StaplesUiComponent } from './staples-ui.component';
 import { ButtonComponent } from './button/button.component';
+import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
   declarations: [StaplesUiComponent, ButtonComponent],
-  imports: [
-  ],
-  exports: [StaplesUiComponent]
+  imports: [CommonModule],
+  exports: [StaplesUiComponent, ButtonComponent]
 })
 export class StaplesUiModule { }
