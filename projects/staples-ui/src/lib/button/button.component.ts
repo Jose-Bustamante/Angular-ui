3a3b9bf512f2daad0906df9f3368fff62b60c560
Jose Bustamante <bercopy@gmail.com>
2021-02-10 13:58:47 +0100

    UI - Stable 2.0

--- projects/staples-ui/src/lib/button/button.component.ts
+++ projects/staples-ui/src/lib/button/button.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'pl-button', // pl is our prefix
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
   @Input('label') label: string | null = 'Click me!';
