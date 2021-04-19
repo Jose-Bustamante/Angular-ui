15cc2452d001679bc9589ecafab1b7b4d804498d
JAN SZAFRAN <jan.szafran@staples-solutions.com>
2021-04-19 13:58:23 +0200

    imported buttons and icons

--- projects/staples-ui/src/lib/button/stories/Button.stories.ts
+++ projects/staples-ui/src/lib/button/stories/Button.stories.ts
@@ -1,27 +1,38 @@
+import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
+ 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+ 
 import { ButtonComponent } from '../button.component';
-
+ 
 export default {
-  title: 'Atomics/Buttons',
-  component: ButtonComponent,
-  argTypes: {
-    label: { control: 'text' },
-    // we need to override here since in Angular it could be null as well and therefore it would become an ambigious data type for storybook
-  }
+ title: 'Atomics/Buttons',
+ component: ButtonComponent,
+ decorators: [
+ moduleMetadata({
+  declarations: [ButtonComponent],
+  imports: [MatButtonModule, MatIconModule]
+ })
+ ],
+ argTypes: {
+ label: { control: 'text' },
+ // we need to override here since in Angular it could be null as well and therefore it would become an ambigious data type for storybook
+ }
 } as Meta;
-
+ 
 const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
-  component: ButtonComponent,
-  props: args,
+ component: ButtonComponent,
+ props: args,
 });
-
+ 
 export const FancyBlueButton = Template.bind({});
 FancyBlueButton.args = {
-  label: 'Button',
+ label: 'Button',
 };
-
+ 
 export const FancyPinkButton = Template.bind({});
 FancyPinkButton.args = {
-  label: 'Pink version',
-  pink: true,
+ label: 'Pink version',
+ pink: true,
 };
