bf569d0922588794062c9fdd634db89f723a3cb3
Jose Bustamante <bercopy@gmail.com>
2021-05-03 11:53:51 +0200

    StaplesUI - Storybook SCSS pre-processor on preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,8 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+// Build and load the Theme SCSS into StoryBook
+import '!style-loader!css-loader!sass-loader!../projects/staples-ui/src/theme.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
