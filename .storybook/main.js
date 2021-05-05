3a6d32dfd7fb7171b134fa97c86df4d97dd5278c
Jose Bustamante <bercopy@gmail.com>
2021-05-05 17:23:19 +0200

    StaplesUI - Did it work?

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../projects/staples-ui/src/lib/**/stories/*.stories.@(js|ts|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    '@storybook/preset-scss'
+  ],
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,8 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+// import '../projects/staples-ui/src/staples-ui.theme.scss';
+
 // Build and load the Theme SCSS into StoryBook
 import '!style-loader!css-loader!sass-loader!../projects/staples-ui/src/staples-ui.theme.scss';
 
