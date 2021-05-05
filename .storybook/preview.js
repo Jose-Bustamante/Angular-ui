55acff17d11c7d778c459c80a5a9b1d63545977e
Jose Bustamante <bercopy@gmail.com>
2021-05-05 17:29:56 +0200

    StaplesUI - Final clean up, Imports everywhere

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,7 +6,7 @@ setCompodocJson(docJson);
 // import '../projects/staples-ui/src/staples-ui.theme.scss';
 
 // Build and load the Theme SCSS into StoryBook
-import '!style-loader!css-loader!sass-loader!../projects/staples-ui/src/staples-ui.theme.scss';
+import '!style-loader!css-loader!sass-loader!../projects/staples-ui/src/styles/staples-ui.theme.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
